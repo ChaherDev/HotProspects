4b0dfdabe1cdcf8d518a8ab20635b47b361004e8
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-10-15 21:10:50 +0200

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Chaher Machhour on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
